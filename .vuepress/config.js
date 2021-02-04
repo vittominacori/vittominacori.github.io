a2323882fdbc4f514adfc2f11cdf528fb980513d
vittominacori <vittoriominacori@gmail.com>
2021-02-04 13:37:47 +0100

    feat: add fb verification

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -8,6 +8,7 @@ module.exports = {
     ['meta', {property: 'twitter:card', content: 'summary_large_image'}],
     ['meta', {property: 'twitter:image', content: 'https://vittominacori.github.io/assets/images/vittominacori.jpg'}],
     ['meta', {name: 'google-site-verification', content: 'Q3pMcwECEGhU_s_cyixqPpPcA8hyrJKEu-vIDEfLAOY'}],
+    ['meta', {name: 'facebook-domain-verification', content: '53g1h8apbdq8dkcc84e2e5lkq1rfym'}],
     ['meta', {name: 'msvalidate.01', content: '2222C2A799471867FB1E317854E92631'}],
     ['script',
       {
