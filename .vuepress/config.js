f3728b8ac1bea8cf922a120b2da28326acb9d489
Vittorio Minacori <vittoriominacori@gmail.com>
2024-02-06 16:15:15 +0100

    fix: update layout

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -10,7 +10,7 @@ module.exports = {
       "meta",
       {
         name: "viewport",
-        content: "width=device-width, initial-scale=1, user-scalable=no",
+        content: "width=device-width, initial-scale=1",
       },
     ],
     ["link", { rel: "icon", href: "/favicon.ico" }],
